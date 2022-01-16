2a05bc623a9e4df9e9115c6d69c83905ccfc2849
Tyler Hillery <hilleryty@yahoo.com>
2022-01-16 10:28:04 -0600

    added custom domain

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://tylerhillery.github.io'
+SITEURL = 'https://tylerhillery.com'
 RELATIVE_URLS = False
 DOMAIN = SITEURL
 HTTPS = True
